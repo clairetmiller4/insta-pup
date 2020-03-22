6d36fa079c566279f9e5c7245e457eac209f222a
Claire Miller <clairetmiller4@gmail.com>
2020-03-22 12:40:01 -0500

    set up project architecture and eslint/prettier

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf'
+};
